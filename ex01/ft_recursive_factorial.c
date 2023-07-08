30c0d18b83c0ade1347416081561a3ed153abeb5
Enee Di Iorio <edi-iori@z3r11p5.42lyon.fr>
2023-07-08 23:05:22 +0200

    add ex02

---
+++ ex01/ft_recursive_factorial.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edi-iori <edi-iori@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/08 22:49:09 by edi-iori          #+#    #+#             */
+/*   Updated: 2023/07/08 23:04:17 by edi-iori         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_factorial(int nb)
+{
+	int	i;
+
+	if (nb < 0)
+	{
+		return (0);
+	}
+	else if (nb == 0 || nb == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return ( nb * ft_recursive_factorial(nb -1));
+	}
+}
+// #include <stdio.h>
+// int main()
+// {
+// 	printf("%d", ft_recursive_factorial(10));
+// }
